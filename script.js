eb39dac37ae0d8b687b78f90398f7f49d5744ba0
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2023-08-21 13:10:40 +0300

    Color squares upon pressing mouse

--- script.js
+++ script.js
@@ -10,10 +10,30 @@ function generateSquares(num) {
     width: ${100 / num}%; 
     height: ${100 / num}%;
     background: white; 
-    border: solid 1px black;
+    border: solid 1px lightgray;
     border-style: dotted;
     `;
   }
 }
+
+let isMouseDown = false;
+
+function startColoring() {
+  isMouseDown = true;
+}
+
+function stopColoring() {
+  isMouseDown = false;
+}
+
+function colorSquares(event) {
+  if (isMouseDown && event.target.classList.contains('content')) {
+    event.target.style.background = 'black';
+  }
+}
+
+document.addEventListener('mousedown', startColoring);
+document.addEventListener('mouseup', stopColoring);
+document.addEventListener('mousemove', colorSquares);
 
 generateSquares(16);
