cb8e4804428683164588a7d48ce0c54e5b94feac
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2024-06-19 20:24:50 +0300

    Set up a hover effect so that the grid divs change color when mouse passes over them

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const board = document.querySelector('.board');
 
 // Get input from user related to number of cells per side of board (16 by default)
-let input = document.querySelector('input');
+const input = document.querySelector('input');
+
+// const enterButton = document.querySelector('.enter-btn');
+// enterButton.addEventListener("click", () => {
+
+// })
 
 let squaresPerSide = Number(input.value);
-console.log(squaresPerSide)
 
 // Find a way to calculate the sides of the each div to prevent overflow from parent container
 
 // Populate board with cells
 function populateBoard (squaresPerSide = 16) {
-  console.log(squaresPerSide)
   let totalnbOfSquares = squaresPerSide*squaresPerSide;
   for(let i = 1; i <= totalnbOfSquares; i++) {
     const div = document.createElement("div");
@@ -23,5 +26,17 @@ function populateBoard (squaresPerSide = 16) {
 populateBoard()
 
 // Add hover effect to change color of divs on board
+// function colorCells() {
+//   debugger
+  let cells = document.querySelectorAll('.cell');
+  cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+      cell.style.backgroundColor = 'black';
+    });
+  });
+// }
+
+// board.addEventListener("click", () => {
+// })
 
 // Allow user to change colors
