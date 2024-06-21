5bc9c5e0c5ce4e99baf293f1e49b40f3c8185e98
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2024-06-21 15:51:00 +0300

    Format code

--- script.js
+++ script.js
@@ -2,31 +2,32 @@ const BOARD_SIDE = 550;
 
 const board = document.querySelector(".board");
 
-// Get input from user related to number of cells per side of board (16 by default)
+const enterButton = document.querySelector(".enter-btn");
+
+const buttonContainer = document.querySelector(".buttons");
+
+const blackButton = document.querySelector(".black");
+
+const randomColors = document.querySelector(".random-colors");
+
+const eraser = document.querySelector(".eraser");
+
+const resetButton = document.querySelector(".reset");
+
 let userInput = document.querySelector("#nb-of-cells");
 
 let cellsPerSide;
 
 let cells = document.querySelectorAll(".cell");
 
-const enterButton = document.querySelector(".enter-btn");
-enterButton.addEventListener("click", () => {
-  if (!userInput) {
-    cellsPerSide = 16;
-  }
-  cellsPerSide = userInput.value;
-  if (cells) {
-    cells.forEach((cell) => {
-      board.removeChild(cell);
-    });
-  }
-  populateBoard();
-  cells = document.querySelectorAll(".cell")
-});
+let color = "black";
 
-// Populate board with cells
+let coloring = false;
+
 function populateBoard() {
+
   let totalNumOfcells = cellsPerSide * cellsPerSide;
+
   for (let i = 1; i <= totalNumOfcells; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
@@ -37,92 +38,128 @@ function populateBoard() {
 
     board.appendChild(cell);
   }
+  
 }
 
-const randomColors = document.querySelector('.random-colors');
+(function chooseColor() {
+
+  const chosenColor = document.querySelector(".chosen-color");
+
+  buttonContainer.addEventListener("click", (e) => {
+    let buttonClass = e.target.classList;
+
+    if (buttonClass.contains("black")) {
+      color = "black";
+      blackButton.style.backgroundColor = "black";
+      blackButton.style.color = "white";
+      eraser.style.backgroundColor = "";
+      randomColors.style.backgroundColor = "";
+
+    } else if (buttonClass.contains("chosen-color")) {
+      color = chosenColor.value;
+      console.log(color);
+      blackButton.style.backgroundColor = "";
+      blackButton.style.color = "";
+      eraser.style.backgroundColor = "";
+      randomColors.style.backgroundColor = "";
+
+    } else if (buttonClass.contains("eraser")) {
+      color = "white";
+      eraser.style.backgroundColor = "white";
+      blackButton.style.backgroundColor = "";
+      blackButton.style.color = "";
+      randomColors.style.backgroundColor = "";
+
+    } else {
+      eraser.style.backgroundColor = "";
+      blackButton.style.backgroundColor = "";
+      blackButton.style.color = "";
+      randomColors.style.backgroundColor = "";
+    }
+
+  });
+})();
 
 function handleClick(e) {
+
+  randomColors.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  blackButton.style.backgroundColor = "";
+  blackButton.style.color = "";
+  eraser.style.backgroundColor = "";
 
   e.stopPropagation();
 
   function handleMouseOver() {
-    console.log(coloring)
+    console.log(coloring);
     if (!coloring) {
-      color = 'white';
+      color = "white";
     } else {
-      color = '#' + Math.floor(Math.random()*16777215).toString(16);
+      color = "#" + Math.floor(Math.random() * 16777215).toString(16);
     }
   }
-  cells.forEach(cell => {
-    cell.addEventListener('mouseover', handleMouseOver)
+
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseover", handleMouseOver);
   });
 
-  buttonContainer.addEventListener('click', e => {
-    if (!(e.target.classList.contains('random-colors'))) {
-      cells.forEach(cell => {
-        cell.removeEventListener('mouseover', handleMouseOver);
-      })
-    }
-  })
-}
-
-randomColors.addEventListener('click', handleClick)
-
-// Allow user to change colors
-let color = "black";
-
-const buttonContainer = document.querySelector(".buttons");
-function chooseColor() {
-  const chosenColor = document.querySelector(".chosen-color");
-
   buttonContainer.addEventListener("click", (e) => {
-    
-    let buttonClass = e.target.classList;
-    // debugger
-    if (buttonClass.contains("black")) {
-      color = "black";
-    } else if (buttonClass.contains("chosen-color")) {
-      color = chosenColor.value;
-      console.log(color);
-      } else if (buttonClass.contains("eraser")) {
-        color = "white";
-      }
-    });
+    if (!e.target.classList.contains("random-colors")) {
+      cells.forEach((cell) => {
+        cell.removeEventListener("mouseover", handleMouseOver);
+      });
+    }
+  });
+
 }
-  console.log(color);
 
-chooseColor();
+randomColors.addEventListener("click", handleClick);
 
-// Add hover effect to change color of divs on board
-// function colorCells() {
-// }
-let coloring = false;
+enterButton.addEventListener("click", () => {
 
-board.addEventListener("click", e => {
+  if (!userInput) {
+    cellsPerSide = 16;
+  }
+
+  cellsPerSide = userInput.value;
+  if (cells) {
+    cells.forEach((cell) => {
+      board.removeChild(cell);
+    });
+  }
+
+  populateBoard();
+
+  cells = document.querySelectorAll(".cell");
+
+});
+
+board.addEventListener("click", (e) => {
+
   if (e.target.classList.contains("cell")) {
+
     if (!coloring) {
       coloring = true;
     } else {
       coloring = false;
     }
+
   }
-  console.log(coloring);
 });
 
-board.addEventListener("mouseover", e => {
+board.addEventListener("mouseover", (e) => {
+
   if (coloring) {
     if (e.target.classList.contains("cell")) {
-      console.log(color);
       e.target.style.backgroundColor = color;
     }
   }
+
 });
 
-//Reset board
-const reset = document.querySelector(".reset");
+resetButton.addEventListener("click", () => {
 
-reset.addEventListener("click", () => {
   cells.forEach((cell) => {
     cell.style.backgroundColor = "white";
   });
+
 });
