6bf2f62757c482cf39d527f074e064633ce54ada
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2024-09-22 09:37:42 +0300

    Refactor code and fix random colors

--- script.js
+++ script.js
@@ -1,166 +1,112 @@
-const BOARD_SIDE = 550;
-
 const board = document.querySelector(".board");
-
 const enterButton = document.querySelector(".enter-btn");
-
 const buttonContainer = document.querySelector(".buttons");
-
 const blackButton = document.querySelector(".black");
-
 const randomColors = document.querySelector(".random-colors");
-
 const eraser = document.querySelector(".eraser");
-
 const resetButton = document.querySelector(".reset");
-
-let userInput = document.querySelector("#nb-of-cells");
-
-let cellsPerSide;
-
-let cells = document.querySelectorAll(".cell");
+const chosenColor = document.querySelector(".chosen-color");
 
 let color = "black";
-
 let coloring = false;
 
-function populateBoard() {
+function populateBoard(cellsPerSide = 16) {
+  // Actual width of board
+  const boardSide = 550;
 
-  let totalNumOfcells = cellsPerSide * cellsPerSide;
-
-  for (let i = 1; i <= totalNumOfcells; i++) {
+  for (let i = 1; i <= cellsPerSide ** 2; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    const cellSideLength = BOARD_SIDE / cellsPerSide;
-    const cellSidePercentage = (cellSideLength * 100) / BOARD_SIDE;
-    cell.style.width = `${cellSidePercentage}%`;
-    cell.style.height = `${cellSidePercentage}%`;
-
+    const cellSideLength = boardSide / cellsPerSide;
+    const cellSidePercentage = (cellSideLength * 100) / boardSide;
+    cell.style.width = cell.style.height = `${cellSidePercentage}%`;
     board.appendChild(cell);
   }
-
 }
 
-function chooseColor(e) {
-  color = e.target.value;
+populateBoard();
+
+function selectRandomColor() {
+  if (!coloring) {
+    color = "white";
+  } else {
+    color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
 }
 
-  const chosenColor = document.querySelector(".chosen-color");
+chosenColor.addEventListener("change", (e) => color = e.target.value);
 
-  chosenColor.addEventListener("change", chooseColor);
+buttonContainer.addEventListener("click", (e) => {
+  let buttonClass = e.target.classList;
 
-  buttonContainer.addEventListener("click", (e) => {
-    let buttonClass = e.target.classList;
-
-    if (buttonClass.contains("black")) {
-      color = "black";
-      blackButton.style.backgroundColor = "black";
-      blackButton.style.color = "white";
-      eraser.style.backgroundColor = "";
-      randomColors.style.backgroundColor = "";
-
-    } else if (buttonClass.contains("chosen-color")) {
-      blackButton.style.backgroundColor = "";
-      blackButton.style.color = "";
-      eraser.style.backgroundColor = "";
-      randomColors.style.backgroundColor = "";
-
-    } else if (buttonClass.contains("eraser")) {
-      color = "white";
-      eraser.style.backgroundColor = "white";
-      blackButton.style.backgroundColor = "";
-      blackButton.style.color = "";
-      randomColors.style.backgroundColor = "";
-
-    } else {
-      eraser.style.backgroundColor = "";
-      blackButton.style.backgroundColor = "";
-      blackButton.style.color = "";
-      randomColors.style.backgroundColor = "";
-    }
-
-  });
-
-function handleClick(e) {
-
-  randomColors.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  blackButton.style.backgroundColor = "";
-  blackButton.style.color = "";
-  eraser.style.backgroundColor = "";
-
-  e.stopPropagation();
-
-  function handleMouseOver() {
-    console.log(coloring);
-    if (!coloring) {
-      color = "white";
-    } else {
-      color = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    }
+  if (!buttonClass.contains("random-colors")) {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+      cell.removeEventListener("mouseover", selectRandomColor);
+    });
   }
 
-  cells.forEach((cell) => {
-    cell.addEventListener("mouseover", handleMouseOver);
-  });
-
-  buttonContainer.addEventListener("click", (e) => {
-    if (!e.target.classList.contains("random-colors")) {
-      cells.forEach((cell) => {
-        cell.removeEventListener("mouseover", handleMouseOver);
-      });
-    }
-  });
-
-}
-
-randomColors.addEventListener("click", handleClick);
+  if (buttonClass.contains("black")) {
+    color = "black";
+    blackButton.style.backgroundColor = "black";
+    blackButton.style.color = "white";
+    eraser.style.backgroundColor = "";
+    randomColors.style.backgroundColor = "";
+  } else if (buttonClass.contains("chosen-color")) {
+    blackButton.style.backgroundColor = "";
+    blackButton.style.color = "";
+    eraser.style.backgroundColor = "";
+    randomColors.style.backgroundColor = "";
+  } else if (buttonClass.contains("random-colors")) {
+    randomColors.style.backgroundColor =
+    "#" + Math.floor(Math.random() * 16777215).toString(16);
+    blackButton.style.backgroundColor = "";
+    blackButton.style.color = "";
+    eraser.style.backgroundColor = "";
+    e.stopPropagation();
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+      cell.addEventListener("mouseover", selectRandomColor);
+    });
+  } else if (buttonClass.contains("eraser")) {
+    color = "white";
+    eraser.style.backgroundColor = "white";
+    blackButton.style.backgroundColor = "";
+    blackButton.style.color = "";
+    randomColors.style.backgroundColor = "";
+  } else {
+    eraser.style.backgroundColor = "";
+    blackButton.style.backgroundColor = "";
+    blackButton.style.color = "";
+    randomColors.style.backgroundColor = "";
+  }
+});
 
 enterButton.addEventListener("click", () => {
-
-  if (!userInput.value) {
-    cellsPerSide = 16;
-  } else {
-    cellsPerSide = userInput.value;
+  const cells = document.querySelectorAll(".cell");
+  if (cells) {
+    cells.forEach((cell) => board.removeChild(cell));
   }
-    if (cells) {
-      cells.forEach((cell) => {
-        board.removeChild(cell);
-      });
-  }
-
-  populateBoard();
-
-  cells = document.querySelectorAll(".cell");
-
+  populateBoard(document.querySelector("#nb-of-cells").value);
 });
 
 board.addEventListener("click", (e) => {
-
   if (e.target.classList.contains("cell")) {
-
-    if (!coloring) {
-      coloring = true;
-    } else {
-      coloring = false;
-    }
-
+    coloring = !coloring;
   }
 });
 
 board.addEventListener("mouseover", (e) => {
-
   if (coloring) {
     if (e.target.classList.contains("cell")) {
       e.target.style.backgroundColor = color;
     }
   }
-
 });
 
 resetButton.addEventListener("click", () => {
-
+  const cells = document.querySelectorAll('.cell');
   cells.forEach((cell) => {
     cell.style.backgroundColor = "white";
   });
-
 });
